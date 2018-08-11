64dddc140bb6bbe47424854ceaeed765aba5e914
Omar <omaaar90@gmail.com>
2018-08-11 18:22:25 +0200

    UI created no controller yet

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,20 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 public class Controller {
-}
+
+
+        @FXML
+        private Button hello;
+
+        @FXML
+        void hellothere(ActionEvent event) {
+Main.sayhello();
+        }
+
+    }
+
+
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
+
 public class Main extends Application {
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 900 , 600));
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+public static void sayhello(){
+    System.out.println("Hello0");
 }
+        public static void main(String[] args) {
+            launch(args);
+        }
+}
+
