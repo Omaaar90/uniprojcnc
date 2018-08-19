bcea8d6ff318145cbcde54c3d94004d8e5316025
Omar <omaaar90@gmail.com>
2018-08-19 11:31:36 +0200

    Thread added with drawing

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,6 +7,8 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
 import javafx.scene.control.TableColumn;
@@ -14,15 +16,25 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
 import javafx.util.Callback;
+import javafx.concurrent.Task;
+
+
 
 import java.util.Map;
 import java.util.TreeMap;
 
+
 public class Controller {
+    private GraphicsContext graphicsContext;
 
         private Map commandMap = new TreeMap();
+        Thread draw;
+        boolean stop = false;
 
+        @FXML
+        Canvas canvas;
         @FXML
         private Button hello;
 
@@ -35,8 +47,18 @@ public class Controller {
         @FXML
         private TableColumn<Map.Entry<String, String>, String> commandColumn;
 
+
+
+
         @FXML
         public void initialize() {
+
+            graphicsContext = canvas.getGraphicsContext2D();
+            graphicsContext.setFill(Color.GRAY);
+            graphicsContext.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+            graphicsContext.setStroke(Color.BLACK);
+            graphicsContext.setLineWidth(2);
+
                 /*
                 commandMap.put("N01","G01");
                 commandMap.put("N02","G02");
@@ -72,18 +94,36 @@ public class Controller {
 
 
         @FXML
-        private void handlePlayAction(ActionEvent event) {
-                Main.sayhello();
+        private void handlePlayAction(ActionEvent event) throws InterruptedException {
+
+            Task draw = new Task<Void>() {
+                @Override
+                public Void call() throws InterruptedException {
+                    for (double i = 1; i < 100; i++) {
+                            graphicsContext.beginPath();
+                            graphicsContext.lineTo(i, 2);
+                            Thread.sleep(100);
+                            graphicsContext.stroke();
+                        if(!stop){
+
+                        }
+                    }
+
+                    return null;
+                }
+            };
+           new Thread(draw).start();
         }
 
         @FXML
         private void handlePauseAction(ActionEvent event) {
-                Main.sayhello();
+
+
         }
 
         @FXML
         private void handleStopAction(ActionEvent event) {
-                Main.sayhello();
+                stop = true;
         }
 
         @FXML
@@ -96,10 +136,9 @@ public class Controller {
                                 addCommandToList(textfield.getText());
                                 textfield.setText("");
                         }
-
-
                 }
         }
+
 
         public Map getCommandMap() {
                 return commandMap;
