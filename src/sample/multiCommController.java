27530ad175454d23f59e051650de16790bd51e33
Omar <omaaar90@gmail.com>
2018-08-19 17:09:42 +0200

    Error with batch command addition fixed

---
+++ src/sample/multiCommController.java
@@ -0,0 +1,27 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class multiCommController{
+    public AnchorPane anchorPane;
+    public Button add;
+    public Button close;
+    @FXML
+    public void handleAddMultiCommAction(ActionEvent event) {
+    }
+
+    public void handleCloseMultiCommAction(ActionEvent event) {
+        // get a handle to the stage
+        Stage stage = (Stage) close.getScene().getWindow();
+        // do what you have to do
+        stage.close();
+
+    }
+
+    public void initialize() {
+    }
+}
